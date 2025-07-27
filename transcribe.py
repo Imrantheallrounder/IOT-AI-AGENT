125129c35209b232ce504cbc21b34650eb6ee95f
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2025-07-27 10:39:04 +0530

    removed ambient_noise and increased pause_threshold time

--- transcribe.py
+++ transcribe.py
@@ -1,19 +1,22 @@
 import speech_recognition as sr
+import time
 
 # Initialize recognizer
 recognizer = sr.Recognizer()
+# timeout time after user query completion
+recognizer.pause_threshold = 1.2
 
 # Function to recognize speech
 def transcribe_audio():
-    # Use the microphone as the audio source
-    with sr.Microphone() as source:
-        print("Please say something...")
-        # Adjust for ambient noise
-        recognizer.adjust_for_ambient_noise(source)
-        # Listen to the audio from the microphone
-        audio = recognizer.listen(source)
-
     try:
+        # Use the microphone as the audio source
+        with sr.Microphone() as source:
+            # Adjust for ambient noise
+            # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            # Listen to the audio from the microphone
+            print("Please say something...")
+            audio = recognizer.listen(source, timeout=2)
+
         # Use the Google Web Speech API to transcribe the audio
         print("Transcribing...")
         text = recognizer.recognize_google(audio)
@@ -23,6 +26,8 @@ def transcribe_audio():
         print("Sorry, I could not understand the audio.")
     except sr.RequestError as e:
         print(f"Could not request results; {e}")
+    except sr.WaitTimeoutError:
+        print("WaitTimeoutError")
         
     return None
 
